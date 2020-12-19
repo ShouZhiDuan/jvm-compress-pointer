4108642bba79fe4b49e01d6e5abadd516f2d10bf
段守志 <shouzhi.duan@nvxclouds.com>
2020-12-19 17:20:05 +0800

    init project

---
+++ src/main/java/com/jvm/jvm/JvmCompressPointerApplication.java
@@ -0,0 +1,13 @@
+package com.jvm.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmCompressPointerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmCompressPointerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jvm/jvm/JvmCompressPointerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvm.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmCompressPointerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
