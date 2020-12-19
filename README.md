432b99cfbc95d2e217945080fb2d11edf94b733b
段守志 <shouzhi.duan@nvxclouds.com>
2020-12-19 17:57:12 +0800

    fix code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# JVM指针探索
+# JVM指针探索(因md不太方便贴图，所以如下都没有给出图，大家可以根据相关工具查看进程所占用的内存大小)
+## 1、你的服务器内存>=32G
+java -Xms32g -Xmx32g -jar jvm-compress-pointer.jar 1000
+可以看到，32g堆内存下，包含1000万个Integer元素的LinkedList共占用内存814.9MB。
+
+## 2、设置31g
+java -Xms31g -Xmx31g -jar jvm-compress-pointer.jar 1000
+内存只占用了495.5MB，整整下降了232.3MB！下降幅度达39.2%.
+
+## 3、通过探索发现
+32g临界点，并不是一个确切的值。只能说在接近32g范围内有可能会导致指针压缩失效。
--- src/main/java/com/jvm/jvm/JvmCompressPointerApplication.java
+++ src/main/java/com/jvm/jvm/JvmCompressPointerApplication.java
@@ -1,13 +1,27 @@
 package com.jvm.jvm;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @SpringBootApplication
 public class JvmCompressPointerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JvmCompressPointerApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("start main function ......");
+        System.out.println("args：" + args[0]);
+        //SpringApplication.run(JvmCompressPointerApplication.class, args);
+        List<Integer> list = new LinkedList<>();
+        int total = Integer.parseInt(args[0]) * 10000;
+        for (int i = 0; i < total; i++) {
+            list.add(i);
+        }
+        System.out.println(list.size());
+        // 留给观察和截图的时间
+        Thread.sleep(6000);
+        // 维持一个引用
+        System.out.println("结束：" + list.size());
     }
 
 }
